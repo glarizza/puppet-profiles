01097737738b5f982ec89987eeedfd9f8b499ceb
Gary Larizza <gary@puppetlabs.com>
2013-09-05 15:35:46 -0700

    0.0.2 - include a wordpress_localhost class
    Previously, the wordpress class assumed name-based lookup and setting
    up a host entry for the node. Because I'm preparing for a Vagrant demo
    which will all be self-contained, I created a class called wordpress_localhost
    that will define everything on localhost and also setup a firewall rule to
    allow all traffic on port 80.  I've also updated the Modulefile to add
    the dependency on the puppetlabs/firewall module.

---
+++ manifests/wordpress_localhost.pp
@@ -0,0 +1,55 @@
+class profiles::wordpress_localhost {
+  # This class creates a Wordpress Install setup to listen on localhost
+  # of a node...which is good for quick-provisioning examples with Vagrant
+  # (hint hint)
+  
+  $wordpress_user_password =  'wordpress'
+  $mysql_root_password     =  'password'
+  $wordpress_db_host       =  'localhost'
+  $wordpress_db_name       =  'wordpress'
+  $wordpress_db_password   =  'wordpress'
+
+
+  ## Create user
+  group { 'wordpress':
+    ensure => present,
+  }
+  user { 'wordpress':
+    ensure   => present,
+    gid      => 'wordpress',
+    password => $wordpress_user_password,
+    home     => '/var/www/wordpress',
+    require  => Package['httpd'],
+  }
+
+  ## Configure mysql
+  class { 'mysql::server':
+    config_hash => { 'root_password' => $wordpress_root_password }
+  }
+  include mysql::php
+
+  ## Configure apache
+  include apache
+  include apache::mod::php
+
+  ## Configure ftp for installing updates/themes. Disallows root login
+  include vsftpd
+
+  ## Configure wordpress
+  class { '::wordpress':
+    install_dir => '/var/www/html',
+    db_name     => $wordpress_db_name,
+    db_host     => $wordpress_db_host,
+    db_user     => 'wordpress',
+    db_password => $wordpress_db_password,
+    wp_owner    => 'wordpress',
+    wp_group    => 'wordpress',
+    require     => Package['httpd'],
+  }
+
+  firewall { '100 allow http access':
+    port   => '80',
+    proto  => 'tcp',
+    action => 'accept',
+  }
+}
