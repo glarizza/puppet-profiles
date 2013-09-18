ab1e5f87248b0dc100261ddc5ccd69d7e47175aa
Gary Larizza <gary@puppetlabs.com>
2013-09-18 14:28:58 +0200

    Stupid namespacing + firewall
    So, I HATE that Puppet namespacing bug - unless I say ::jenkins,
    it's going to relatively declare a jenkins class...in this case, our
    class is called 'profiles::jenkins' and an 'include jenkins' is going
    to look relative to the profiles class and try to declare the current
    class we're in...hence, profiles::jenkins.  When I say 'include ::jenkins'
    it will look for a jenkins class in top scope.  Scumbag Puppet.
    
    Also, let's add a Firewall rule for port 8080 just to have fun.

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,8 +1,13 @@
 class profiles::jenkins {
-  include jenkins
+  include ::jenkins
   class { 'java':
     distribution => 'jdk',
     version      => 'latest',
     before       => Class['jenkins'],
+  }
+  firewall { '100 allow jenkins access':
+    port   => '8080',
+    proto  => 'tcp',
+    action => 'accept',
   }
 }
