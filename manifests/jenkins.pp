6ac68875f23b7c5d6c658ac9af7bef3e6db31242
Gary Larizza <gary@puppetlabs.com>
2014-11-07 15:15:14 +0000

    Update Jenkins
    This commit aligns the module with the puppetlabs/java module and according
    to recent changes to rtyler/jenkins.

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,13 +1,23 @@
 class profiles::jenkins {
-  include ::jenkins
-  class { 'java':
-    distribution => 'jdk',
-    version      => 'latest',
-    before       => Class['jenkins'],
+  # Set variables. I'm using the 'default' second value
+  # for demonstration purposes only in case I need to run
+  # this demo on a system that doesn't have Hiera configured.
+  $jenkins_port = hiera('jenkins_port', '9091')
+  $java_dist    = hiera('java_dist', 'jdk')
+  $java_version = hiera('java_version', 'latest')
+
+  class { '::jenkins':
+    configure_firewall => true,
+    install_java       => false,
+    port               => $jenkins_port,
+    config_hash        => {
+      'JENKINS_PORT' => { 'value' => $jenkins_port },
+    },
   }
-  firewall { '100 allow jenkins access':
-    port   => '8080',
-    proto  => 'tcp',
-    action => 'accept',
+
+  class { '::java':
+    distribution => $java_dist,
+    version      => $java_version,
+    before       => Class['jenkins'],
   }
 }
